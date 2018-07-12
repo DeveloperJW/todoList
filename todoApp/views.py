28b59a1589e84190f56202c5028a9cd3e666dc0a
DeveloperJW <zpw1992@gmail.com>
2018-07-12 16:57:20 -0500

    Added feature of adding and deleting

--- todoApp/views.py
+++ todoApp/views.py
@@ -1,13 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import List
+from .forms import ListForm
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+
 
 # Create your views here.
 def home(request):
-    all_items = List.objects.all
-    return render(request, 'home.html', {'all_items':all_items})
+
+    if request.method == 'POST':
+        form = ListForm(request.POST or None)
+
+        if form.is_valid():
+            form.save()
+            all_items = List.objects.all
+            messages.success(request, 'Okay! An item has been added to the List!')
+            return render(request, 'home.html', {'all_items': all_items})
+    else:
+        all_items = List.objects.all
+        return render(request, 'home.html', {'all_items': all_items})
 
 
 def about(request):
-    my_name="Justin Wang"
+    my_name = "Justin Wang"
 
     return render(request, 'about.html', {'name': my_name})
+
+def delete(request, list_id):
+    item = List.objects.get(pk=list_id)
+    item.delete()
+    messages.success(request, 'An item has been Deleted.')
+    return redirect('home')
+
