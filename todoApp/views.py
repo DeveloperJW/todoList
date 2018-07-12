6a9c327b80d723731e340f61c3ce3638d1fe8e51
DeveloperJW <zpw1992@gmail.com>
2018-07-12 15:25:54 -0500

    Try to put the list of items from database to homepage

--- todoApp/views.py
+++ todoApp/views.py
@@ -3,7 +3,8 @@ from .models import List
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    all_items = List.objects.all
+    return render(request, 'home.html', {'all_items':all_items})
 
 
 def about(request):
