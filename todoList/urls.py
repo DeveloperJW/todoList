869b8a9b2f0603ed494e0d219fa2f0920fc199ce
DeveloperJW <zpw1992@gmail.com>
2018-07-12 14:16:30 -0500

    set up template using Django; setting url for home page and about us page.

--- todoList/urls.py
+++ todoList/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from todoApp import views
 
 urlpatterns = [
+    # default path to admin panel of Django
     path('admin/', admin.site.urls),
+    # set up root urls
+    path('', include('todoApp.urls')),
 ]
